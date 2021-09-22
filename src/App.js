99404c6c0f9a29afe5961f636315ac4e6bfa91c8
pearlkothari <pearlkothari2@gmail.com>
2021-09-22 17:03:50 +0530

    commit to main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from '../src/Header/Header.jsx';
+import Home from '../src/Home/Home.jsx'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Home></Home>
     </div>
   );
 }
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,32 @@
 import React,{Component} from 'react';
+import {BrowserRouter as Router,Link} from "react-router-dom";
+import './Home.css';
 
 class Home extends Component{
     render(){
         return(
             <div className="Home__page">
-
+                <div clasName="Home__Prime__Container">
+                    <a href="https://www.primevideo.com">
+                        <img className="Prime__Image" src="https://store-images.s-microsoft.com/image/apps.16285.14618985536919905.552c0017-6644-49a8-8467-8f7b34ce0428.30ad6b05-16d9-4d5e-a242-43107708a16a?mode=scale&q=90&h=1080&w=1920"
+                            width="100%" height="80%"
+                            alt=""
+                        />
+                    </a>
+                </div>
+                <div className="home__row">
+                    
+                </div>
+                <div className="home__row">
+                    
+                </div>
+                <div className="home__row">
+                    
+                </div>
             </div>
         )
     }
+
 }
 
 export default Home;
