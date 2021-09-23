bd899af00b7a38cdc2acc722b5427fca8eae2b73
pearlkothari <pearlkothari2@gmail.com>
2021-09-23 16:58:34 +0530

    commit to main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Header from '../src/Header/Header.jsx';
 import Home from '../src/Home/Home.jsx'
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import { Component } from 'react';
+import ErrorComponent from './ErrorComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <Home></Home>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path='/' exact>
+              <Header></Header>
+              <Home></Home>
+            </Route>
+            <Route component={ErrorComponent}></Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/ErrorComponent.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class ErrorComponent extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Error 402 Page Not Found!!</h1>
+            </div>
+        )
+    };
+}
+
+export default ErrorComponent;
